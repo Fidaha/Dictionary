5d97905e1821e61faa8253b3c60b2d1674d04b54
Fidaha <81554525+Fidaha@users.noreply.github.com>
2023-06-13 18:19:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Dictionary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Dictionary<char, string> letterDictionary = new Dictionary<char, string>();
+        letterDictionary['a'] = "word1";
+        letterDictionary['b'] = "word2";
+        letterDictionary['c'] = "word3";
+
+        foreach(KeyValuePair<char,string> kvp in letterDictionary)
+        {
+           // Console.WriteLine(kvp);
+            Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            
+        }
+
+        Console.WriteLine("");
+        letterDictionary.Remove('c');
+
+        
+        foreach(KeyValuePair<char,string> kvp in letterDictionary)
+        {
+           // Console.WriteLine(kvp);
+            Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            
+        }
+    }
+}
